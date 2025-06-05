5f7daf89509d35ce45c04bad5c9de9ca98f5db3d
Braian Henrique Rodrigues Silva <2023105070055@aluno.ifro.edu.br>
2025-06-05 09:31:54 -0400

    introducao

---
+++ loops.ts
@@ -0,0 +1,17 @@
+// loops
+
+//for
+for (let i = 0; i < 3; i++){
+    console.log(i)
+}
+// wilhw
+let j; number= 0;
+while(j < 3){
+    console.log(j);
+    j++
+}
+let y; number=0;
+do{
+    console.log(y)
+    y++
+}
